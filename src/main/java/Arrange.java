cef1e9eeb94755ab924b2b753ae3c0cb682b37c7
XIECI <cisnoc.xie@oocl.com>
2018-08-23 14:27:44 +0800

    [leo cisnoc] refactor pass

--- src/main/java/Arrange.java
+++ src/main/java/Arrange.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Arrange {
-    public static List<String> cal(String input) {
+    public static List<String> cal_old(String input) {
         if (input.length() == 2) {
             return Arrays.asList(input, input.substring(1, 2) + input.substring(0, 1));
         } else if (input.length() ==1) {
@@ -18,5 +19,24 @@ public class Arrange {
             result.add(input.substring(2, 3) + input.substring(1,2) + input.substring(0,1));
             return result;
         }
+    }
+
+    public static List<String> cal(String input) {
+        if (input.length() ==1) {
+            return Collections.singletonList(input);
+        } else {
+            List<String> result = new ArrayList<>();
+            for (int index = 0; index < input.length(); index++) {
+                List<String> subPermutations = cal(removeOneCharByIndex(input, index));
+                for (String subPermutation : subPermutations) {
+                    result.add(input.charAt(index)+subPermutation);
+                }
+            }
+            return  result;
+        }
+    }
+
+    public static String  removeOneCharByIndex(String input,int index){
+        return input.substring(0,index)+input.substring(index+1);
     }
 }
